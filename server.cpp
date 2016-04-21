74e42344bb49b4074e7c2045a9cc1fef848accb7
xkrat <krajnakmatto@gmail.com>
2016-04-21 18:03:20 +0200

    server args parsed, also tweked some functions and comments

--- server.cpp
+++ server.cpp
@@ -13,19 +13,56 @@
 using namespace std;
 
 
+/*
+* HELP
+*/
+void help()
+{
+  printf("Simple File Server:\n\tUSAGE: server -p <port_number>\n" );
+}
+/*
+*   Handles communication between server and client
+*/
+void handle_communication(int server_socket)
+{
+  if ((listen(server_socket, 1)) < 0) // listen for incomming connection
+  {
+    perror("ERROR: listen");
+    exit(EXIT_FAILURE);
+  }
+
+  while(1)
+  {
+    struct sockaddr_in client;              //new client address
+    socklen_t client_len = sizeof(client);  //size
+		int comm_socket = 0;
+		if((comm_socket = accept(server_socket, (struct sockaddr*)&client, &client_len)) > 0)
+		{
+			printf("INFO: New connection:\n");
+      char buff[1024];
+			int res = 0;
+      while((res = recv(comm_socket, buff, 1024,0)) > 0){   //handle data
+				send(comm_socket, buff, strlen(buff), 0);
+			}
+		}
+    close(comm_socket);
+    printf("Connection closed\n");
+	}
+}
+
 /**
 * INITIOALIZING connection
 */
-void init_connection()
+void init_server(int port)
 {
-  int port = 6666;
-  int connection;
-  int server_socket;
-  // create socket
-  if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) { // creating socket
+
+  int server_socket = 0;
+  if((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)  // creating socket
+  {
     perror("ERR: socket");
     exit(EXIT_FAILURE);
   }
+
   // prepare address for bind
   struct sockaddr_in sw;            //setting up server address
   memset(&sw, 0, sizeof(sw));       // setting up struct for connect
@@ -33,52 +70,32 @@ void init_connection()
   sw.sin_addr.s_addr = INADDR_ANY; // setting properly destination ip address
   sw.sin_port = htons(port);              // set destination port
   //bind network socket to socket file descriptor
+
+  int connection = 0;
   if((connection = bind (server_socket, (struct sockaddr *)&sw, sizeof(struct sockaddr))) < 0 )
   {
     perror("ERR: socket");
     exit(EXIT_FAILURE);
   }
-  // listen for incomming connection
-  if ((listen(server_socket, 1)) < 0)
-	{
-		perror("ERROR: listen");
-		exit(EXIT_FAILURE);
-	}
-  while(1)
-	{
-    struct sockaddr_in client;
-    socklen_t client_len = sizeof(client);
-		int comm_socket = accept(server_socket, (struct sockaddr*)&client, &client_len);
-		if (comm_socket > 0)
-		{
-			printf("INFO: New connection:\n");
-
-			char buff[1024];
-			int res = 0;
-			for (;;)
-			{
-				res = recv(comm_socket, buff, 1024,0);
-                if (res <= 0)
-                    break;
-
-			    send(comm_socket, buff, strlen(buff), 0);
-			}
-		}
-		else
-		{
-			printf(".");
-		}
-		printf("Connection closed\n");
-		close(comm_socket);
-	}
-  printf("Done\n" );
+  cout<< "Hello server" <<endl;
+  handle_communication(server_socket);
 }
 /*
 * MAIN
 */
 int main(int argc, char const *argv[])
 {
-  cout<< "Hello server" <<endl;
-  init_connection();
+  if (argc == 2  && (strcmp(argv[1],"--help") == 0))  {
+    help();
+    exit(EXIT_SUCCESS);
+  }
+  if(argc != 3 || (strcmp(argv[1],"-p") != 0))
+  {
+    fprintf(stderr, "Invalid args\n" );
+    help();
+    exit(EXIT_FAILURE);
+  }
+
+  init_server((int)strtod(argv[2],NULL));
   return 0;
 }
